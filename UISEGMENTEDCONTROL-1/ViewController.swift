e048147b20efdeb23fc1bb3874176abe943cfd70
Syed.Reshma Ruksana <reshmasmile468@achiever-8.local>
2019-10-01 11:46:29 +0530

    Initial Commit

---
+++ UISEGMENTEDCONTROL-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UISEGMENTEDCONTROL-1
+//
+//  Created by Syed.Reshma Ruksana on 01/10/19.
+//  Copyright © 2019 Syed.Reshma Ruksana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
